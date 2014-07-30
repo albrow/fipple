d7aeac8132c12351ad95988068446c2b787d4c94
albrow <stephenalexbrowne@gmail.com>
2014-07-29 23:45:07 -0400

    Initial commit. Basic http response recording and testing.

---
+++ recorder.go
@@ -0,0 +1,99 @@
+package fipple
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+type Recorder struct {
+	t       *testing.T
+	client  *http.Client
+	baseURL string
+}
+
+func NewRecorder(t *testing.T, baseURL string) *Recorder {
+	return &Recorder{
+		t:       t,
+		client:  &http.Client{},
+		baseURL: baseURL,
+	}
+}
+
+func (r *Recorder) newResponse(resp *http.Response) *Response {
+	return &Response{
+		Response: resp,
+		recorder: r,
+	}
+}
+
+func (r *Recorder) newRequest(method string, path string) *http.Request {
+	fullURL := r.baseURL + path
+	req, err := http.NewRequest(method, fullURL, nil)
+	if err != nil {
+		r.t.Fatal(err)
+	}
+	return req
+}
+
+func (r *Recorder) newRequestWithData(method string, path string, data map[string]string) *http.Request {
+	fullURL := r.baseURL + path
+	v := url.Values{}
+	for key, value := range data {
+		v.Add(key, value)
+	}
+	req, err := http.NewRequest(method, fullURL, strings.NewReader(v.Encode()))
+	if err != nil {
+		r.t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return req
+}
+
+// Get sends req and records the results into a fipple.Response.
+// Note that because an http.Request should have already been created
+// with a full, valid url, the baseURL of the Recorder will not be prepended
+// to the url for req. You can run methods on the response to check
+// the results.
+func (r *Recorder) Do(req *http.Request) *Response {
+	httpResp, err := r.client.Do(req)
+	if err != nil {
+		r.t.Fatal(err)
+	}
+	resp := r.newResponse(httpResp)
+	resp.ReadBody()
+	return resp
+}
+
+// Get sends a GET request to the given path and records the results into
+// a fipple.Response. You can run methods on the response
+// to check the results.
+func (r *Recorder) Get(path string) *Response {
+	req := r.newRequest("GET", path)
+	return r.Do(req)
+}
+
+// Post sends a POST request to the given path using the given data as post
+// parameters and records the results into a fipple.Response. You
+// can run methods on the response to check the results.
+func (r *Recorder) Post(path string, data map[string]string) *Response {
+	req := r.newRequestWithData("POST", path, data)
+	return r.Do(req)
+}
+
+// Put sends a PUT request to the given path using the given data as
+// parameters and records the results into a fipple.Response. You
+// can run methods on the response to check the results.
+func (r *Recorder) Put(path string, data map[string]string) *Response {
+	req := r.newRequestWithData("PUT", path, data)
+	return r.Do(req)
+}
+
+// Delete sends a DELETE request to the given path and records the results
+// into a fipple.Response. You can run methods on the response to check the
+// results.
+func (r *Recorder) Delete(path string) *Response {
+	req := r.newRequest("DELETE", path)
+	return r.Do(req)
+}
---
+++ response.go
@@ -0,0 +1,79 @@
+package fipple
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/wsxiaoys/terminal/color"
+	"net/http"
+	"strings"
+)
+
+// Response represents the response from an http request and has methods to
+// make testing easier.
+type Response struct {
+	*http.Response
+	Body     string
+	recorder *Recorder
+	printed  bool
+}
+
+// ReadBody reads r.Response.Body into r.Body. If the content-type is json,
+// the body is automatically indented.
+func (r *Response) ReadBody() {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Response.Body)
+	body := buf.String()
+	// Detect Content-Type and auto-indent if json
+	if strings.Contains(r.Header.Get("Content-Type"), "application/json") {
+		indBuf := new(bytes.Buffer)
+		json.Indent(indBuf, []byte(body), "", "\t")
+		body = indBuf.String()
+	}
+	r.Body = body
+}
+
+// AssertOk causes a test error if response code != 200
+func (r *Response) AssertOk() {
+	r.AssertCode(200)
+}
+
+// AssertCode causes a test error if response code != the given code
+func (r *Response) AssertCode(code int) {
+	if r.StatusCode != code {
+		r.PrintErrorOnce()
+		r.recorder.t.Errorf("Expected response code %d but got: %d", code, r.StatusCode)
+	}
+}
+
+// AssertBodyContains causes a test error if the response body does
+// not contain the given string.
+func (r *Response) AssertBodyContains(str string) {
+	if !strings.Contains(r.Body, str) {
+		r.PrintErrorOnce()
+		r.recorder.t.Errorf("Expected response to contain \"%s\" but it did not.", str)
+	}
+}
+
+// PrintError prints some information about the response as a test error.
+func (r *Response) PrintError() {
+	r.recorder.t.Errorf("%s request to %s failed. Response was: \n%s",
+		r.Request.Method,
+		r.Request.URL.Path,
+		r.ColorBody())
+}
+
+// PrintErrorOnce will only print the response if it has not already been printed.
+// Useful in cases where there are multiple assertions called on the same response and
+// we don't want to repeatedly print out the response for each assertion failure.
+func (r *Response) PrintErrorOnce() {
+	if !r.printed {
+		r.PrintError()
+		r.printed = true
+	}
+}
+
+// ColorBody returns a colorized version of the response body.
+// By default the color is dark grey-ish.
+func (r *Response) ColorBody() string {
+	return color.Sprintf("@{.}%s", r.Body)
+}
